ae89f35685021653c5dc556ff6e40aa3910e782f
Rust Matias <rustmatiasbus@gmail.com>
2022-05-24 10:50:15 -0300

    intento de main page, overlays

--- TP2/EntregaFinal/js/interactions.js
+++ TP2/EntregaFinal/js/interactions.js
@@ -1,15 +1,53 @@
 "use-strict"
 
-
-
-
 document.querySelector("#showLogin").addEventListener( "click", ()=>{
 
     let loginoverlay = document.querySelector("#login");
     let overlay = document.querySelector("#overlay");
 
-    console.log(document.querySelector("login"));
     loginoverlay.classList.toggle("show");
     overlay.classList.toggle("show");
 
 }  ) 
+document.querySelector("#closeLogin").addEventListener( "click", ()=>{
+
+    let loginoverlay = document.querySelector("#login");
+    let overlay = document.querySelector("#overlay");
+
+    loginoverlay.classList.toggle("show");
+    overlay.classList.toggle("show");
+}  ) 
+
+
+
+
+document.querySelector("#goToRegister").addEventListener( "click", ()=>{
+
+    let registeroverlay = document.querySelector("#register");
+
+    registeroverlay.classList.toggle("show");
+}  ) 
+document.querySelector("#closeRegister").addEventListener( "click", ()=>{
+
+    let registeroverlay = document.querySelector("#register");
+    registeroverlay.classList.toggle("show");
+    let loginoverlay = document.querySelector("#login");
+    let overlay = document.querySelector("#overlay");
+    loginoverlay.classList.toggle("show");
+    overlay.classList.toggle("show");
+
+}  ) 
+
+document.querySelector("#goToLogin").addEventListener( "click", ()=>{
+
+    let registeroverlay = document.querySelector("#register");
+    registeroverlay.classList.toggle("show");
+}  ) 
+
+
+document.querySelector("#openBusqueda").addEventListener( "click", ()=>{
+
+    let busqueda = document.querySelector("#busqueda");
+    busqueda.classList.toggle("show");
+}  ) 
+
