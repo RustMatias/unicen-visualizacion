f37c41125c3d2fee6e48903c84ea89957dda7c5c
Rust Matias <rustmatiasbus@gmail.com>
2022-05-25 23:08:04 -0300

    busqueda realizada

--- TP2/EntregaFinal/js/interactions.js
+++ TP2/EntregaFinal/js/interactions.js
@@ -84,15 +84,79 @@ document.querySelector("#closeBusqueda").addEventListener( "click", ()=>{
 
 }) 
 
-document.querySelector("#openGameInfo").addEventListener( "click", ()=>{
+if(document.querySelector("#openGameInfo") != null){
+    document.querySelector("#openGameInfo").addEventListener( "click", ()=>{
 
     
-    //agarra del dom
-    let gameInfo = document.querySelector("#gameInfoPopup");
-    console.log(gameInfo);
-    //agrega la clase
-    gameInfo.classList.toggle("show");
+        //agarra del dom
+        let gameInfo = document.querySelector("#gameInfoPopup");
+        console.log(gameInfo);
+        //agrega la clase
+        gameInfo.classList.toggle("show");
+    
+    }) 
+}
 
+
+
+document.querySelector("#buscador").addEventListener( "change", ()=>{
+
+
+    console.log("pepe")
+    let html = document.querySelector("#viewGamesSearch")
+    let value = document.querySelector("#buscador").value;
+
+    html.innerHTML = ` <div class="view-browser">
+                            <a href="">` + 'Search results for "' + value + `" :</a>
+                            
+                            <div class="visualization">
+                                <div class="contenedor cont-browser">
+                                    <figure>
+                                        <img src="img/game1.jpg" alt="">
+                                        <div class="capa">
+                                            <h2>Nombre del juego</h2>                
+                                        </div>
+                                    </figure>
+                                    
+                                    <figure>
+                                        <img src="img/game2.jpg" alt="">
+                                        <div class="capa">
+                                            <h2>Nombre del juego</h2>                
+                                        </div>
+                                    </figure>
+                                    
+                                </div>
+                                <div class="contenedor cont-browser">
+                                    <figure>
+                                        <img src="img/game2.jpg" alt="">
+                                        <div class="capa">
+                                            <h2>Nombre del juego</h2>                
+                                        </div>
+                                    </figure>
+                                    <figure>
+                                        <img src="img/game3.jpg" alt="">
+                                        <div class="capa">
+                                            <h2>Nombre del juego</h2>                
+                                        </div>
+                                    </figure>
+                                    
+                                </div>
+                                <div class="contenedor cont-browser">
+                                    <figure>
+                                        <img src="img/game2.jpg" alt="">
+                                        <div class="capa">
+                                            <h2>Nombre del juego</h2>                
+                                        </div>
+                                    </figure>
+                                    <figure>
+                                        <img src="img/game3.jpg" alt="">
+                                        <div class="capa">
+                                            <h2>Nombre del juego</h2>                
+                                        </div>
+                                    </figure>
+                                    
+                                </div>
+                                
+                            </div> ` 
 }) 
-
 
